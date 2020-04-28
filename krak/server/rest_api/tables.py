41731a88ed42d671aa1920f182ddb718d29fe945
M. Yetisir <yetisir@gmail.com>
2020-04-28 03:18:09 -0700

    temp progress

--- krak/server/rest_api/tables.py
+++ krak/server/rest_api/tables.py
@@ -7,8 +7,8 @@ from sqlalchemy.schema import CreateTable
 Base = declarative_base()
 
 
-class Boreholes(Base):
-    __tablename__ = 'boreholes'
+class Borehole(Base):
+    __tablename__ = 'borehole'
 
     borehole_id = sql.Column(sql.Integer(), primary_key=True)
     name = sql.Column(sql.String(64), nullable=False)
@@ -17,56 +17,48 @@ class Boreholes(Base):
     z = sql.Column(sql.Float(), nullable=False)
 
 
-class Surveys(Base):
-    __tablename__ = 'surveys'
+class Survey(Base):
+    __tablename__ = 'survey'
 
     survey_id = sql.Column(sql.Integer(), primary_key=True)
     borehole_id = sql.Column(
-            sql.Integer(), sql.ForeignKey('boreholes.borehole_id'))
+            sql.Integer(), sql.ForeignKey('borehole.borehole_id'))
     depth = sql.Column(sql.Float(), nullable=False)
     survey_id = sql.Column(sql.Integer(), primary_key=True, nullable=False)
     azimuth = sql.Column(sql.Float(), nullable=False)
     dip = sql.Column(sql.Float(), nullable=False)
 
 
-class Lithologies(Base):
-    __tablename__ = 'lithologies'
+class Lithology(Base):
+    __tablename__ = 'lithology'
 
     lithology_id = sql.Column(sql.Integer(), primary_key=True)
     name = sql.Column(sql.String(64), nullable=False)
 
 
-class Lithology_logs(Base):
-    __tablename__ = 'lithology_logs'
+class Lithology_log(Base):
+    __tablename__ = 'lithology_log'
 
     borehole_id = sql.Column(
-        sql.Integer(), sql.ForeignKey('boreholes.borehole_id'),
+        sql.Integer(), sql.ForeignKey('borehole.borehole_id'),
         primary_key=True)
     depth_from = sql.Column(sql.Float(), primary_key=True)
     depth_to = sql.Column(sql.Float(), nullable=False)
     lithology_id = sql.Column(
-            sql.Integer(), sql.ForeignKey('lithologies.lithology_id'),
+            sql.Integer(), sql.ForeignKey('lithology.lithology_id'),
             nullable=False)
     comments = sql.Column(sql.String(1024))
 
 
-class Photos(Base):
-    __tablename__ = 'photos'
-
-    photo_id = sql.Column(sql.Integer(), primary_key=True)
-    path = sql.Column(sql.String(1024), nullable=False)
-
-
-class PhotoLogs(Base):
-    __tablename__ = 'photo_logs'
+class CorePhotoLog(Base):
+    __tablename__ = 'photo_log'
 
     borehole_id = sql.Column(
-        sql.Integer(), sql.ForeignKey('boreholes.borehole_id'),
+        sql.Integer(), sql.ForeignKey('borehole.borehole_id'),
         primary_key=True)
     depth_from = sql.Column(sql.Float(), primary_key=True)
     depth_to = sql.Column(sql.Float(), nullable=False)
-    photo_id = sql.Column(
-            sql.Integer(), sql.ForeignKey('photos.photo_id'), nullable=False)
+    path = sql.Column(sql.String(1024), nullable=False)
     comments = sql.Column(sql.String(1024))
 
 
