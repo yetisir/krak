2ec2358c242cc9d8abbaa7499fc60d81e769458a
M. Yetisir <yetisir@gmail.com>
2020-06-18 10:47:33 -0700

    bumps version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='krak',
-    version='0.1.5',
+    version='0.1.6',
     description='KraK Python Library',
     url='http://github.com/yetisir/krak',
     author='M.Yetisir',
