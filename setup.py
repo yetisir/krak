299629ab75a53fc48210b991bbb1d963b87f05f2
M. Yetisir <yetisir@gmail.com>
2020-06-18 10:41:41 -0700

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='krak',
-    version='0.1.4',
+    version='0.1.5',
     description='KraK Python Library',
     url='http://github.com/yetisir/krak',
     author='M.Yetisir',
